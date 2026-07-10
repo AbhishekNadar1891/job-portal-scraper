36150ecd92c5a7d539ffcf88fb469e84a388fe5e
Abhishek Nadar <nadarbalaabhishek@gmail.com>
2026-07-10 12:21:38 +0530

    Add multi-page Naukri scraping

--- scraper/remoteok_scraper.py
+++ scraper/remoteok_scraper.py
@@ -1,7 +1,7 @@
 from bs4 import BeautifulSoup
 
 from scraper.http_client import HttpClient
-from config import BASE_URL
+from config import BASE_URL, MAX_PAGES
 
 
 class RemoteOKScraper:
@@ -11,59 +11,65 @@ class RemoteOKScraper:
 
     def scrape(self):
 
-        html = self.client.get(BASE_URL)
-
-        soup = BeautifulSoup(html, "lxml")
-
-        jobs = soup.find_all("div", class_="srp-jobtuple-wrapper")
-
-        print(f"Jobs Found : {len(jobs)}")
-        print("=" * 100)
-
-        # This list will store every job
         all_jobs = []
 
-        for job in jobs:
+        for page in range(1, MAX_PAGES + 1):
 
-            title = "N/A"
-            company = "N/A"
-            experience = "N/A"
-            location = "N/A"
-            link = "N/A"
-            skills = []
+            if page == 1:
+                url = BASE_URL
+            else:
+                url = BASE_URL.replace("python-jobs", f"python-jobs-{page}")
 
-            title_tag = job.find("a", class_="title")
-            if title_tag:
-                title = title_tag.get_text(strip=True)
-                link = title_tag.get("href")
+            print(f"\nScraping Page {page}")
+            print(url)
 
-            company_tag = job.find("a", class_="comp-name")
-            if company_tag:
-                company = company_tag.get_text(strip=True)
+            html = self.client.get(url)
 
-            exp_tag = job.find("span", class_="expwdth")
-            if exp_tag:
-                experience = exp_tag.get_text(strip=True)
+            soup = BeautifulSoup(html, "lxml")
 
-            location_tag = job.find("span", class_="locWdth")
-            if location_tag:
-                location = location_tag.get_text(strip=True)
+            jobs = soup.find_all("div", class_="srp-jobtuple-wrapper")
 
-            tag_elements = job.find_all("li", class_="tag-li")
-            skills = [tag.get_text(strip=True) for tag in tag_elements]
+            print(f"Jobs Found : {len(jobs)}")
+            print("=" * 100)
 
-            # Create one job record
-            job_data = {
-                "title": title,
-                "company": company,
-                "experience": experience,
-                "location": location,
-                "skills": skills,
-                "link": link
-            }
+            for job in jobs:
 
-            # Store it in the list
-            all_jobs.append(job_data)
+                title = "N/A"
+                company = "N/A"
+                experience = "N/A"
+                location = "N/A"
+                link = "N/A"
+                skills = []
 
-        # Return the complete list of jobs
+                title_tag = job.find("a", class_="title")
+                if title_tag:
+                    title = title_tag.get_text(strip=True)
+                    link = title_tag.get("href")
+
+                company_tag = job.find("a", class_="comp-name")
+                if company_tag:
+                    company = company_tag.get_text(strip=True)
+
+                exp_tag = job.find("span", class_="expwdth")
+                if exp_tag:
+                    experience = exp_tag.get_text(strip=True)
+
+                location_tag = job.find("span", class_="locWdth")
+                if location_tag:
+                    location = location_tag.get_text(strip=True)
+
+                tag_elements = job.find_all("li", class_="tag-li")
+                skills = [tag.get_text(strip=True) for tag in tag_elements]
+
+                job_data = {
+                    "title": title,
+                    "company": company,
+                    "experience": experience,
+                    "location": location,
+                    "skills": skills,
+                    "link": link
+                }
+
+                all_jobs.append(job_data)
+
         return all_jobs
