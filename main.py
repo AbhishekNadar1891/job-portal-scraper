08d7d9805bc1020e1a84a68c6ba0e1a28ffba0f3
Abhishek Nadar <nadarbalaabhishek@gmail.com>
2026-07-10 14:01:06 +0530

    Integrate logging into main application

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
 from scraper.remoteok_scraper import RemoteOKScraper
 from storage.csv_storage import CSVStorage
 
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
+
 
 def main():
 
-    keyword = input("Enter job keyword: ").strip().lower()
+    try:
 
-    scraper = RemoteOKScraper()
+        keyword = input("Enter job keyword: ").strip().lower()
 
-    jobs = scraper.scrape(keyword)
+        logger.info(f"Started scraping for keyword: {keyword}")
 
-    storage = CSVStorage()
+        scraper = RemoteOKScraper()
 
-    storage.save(jobs, f"{keyword}_jobs.csv")
+        jobs = scraper.scrape(keyword)
+
+        logger.info(f"Scraped {len(jobs)} jobs")
+
+        storage = CSVStorage()
+
+        storage.save(jobs, f"{keyword}_jobs.csv")
+
+        logger.info("CSV exported successfully")
+
+    except Exception:
+
+        logger.exception("Unexpected error occurred")
+
+        raise
 
 
 if __name__ == "__main__":
